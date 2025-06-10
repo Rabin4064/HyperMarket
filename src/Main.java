acbceaaaf415f35351390fc853003bd739fde9f3
rabin <mobinrangsa4064@gmail.com>
2025-06-10 11:07:18 +0330

    some sentences updated

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import DevPackage.Service;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Sir.");
-        System.out.println("Welcome to Naddaf HyperMarket!");
+        System.out.println("Welcome to HyperMarket!");
         System.out.println("Have a  good shopping.");
         System.out.println();
 //        Service service = new Service();
